96cfc222f86a5f6043f4f2423520129cc58009ee
FiloPL <tzegarlicki@gmail.com>
2022-09-26 15:19:10 +0200

    #09 - update ProjectService

--- src/main/java/ttsw/filopl/todoapp/logic/ProjectService.java
+++ src/main/java/ttsw/filopl/todoapp/logic/ProjectService.java
@@ -3,10 +3,7 @@ package ttsw.filopl.todoapp.logic;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
 import ttsw.filopl.todoapp.TaskConfigurationProperties;
-import ttsw.filopl.todoapp.model.Project;
-import ttsw.filopl.todoapp.model.ProjectRepository;
-import ttsw.filopl.todoapp.model.Task;
-import ttsw.filopl.todoapp.model.TaskGroupRepository;
+import ttsw.filopl.todoapp.model.*;
 import ttsw.filopl.todoapp.model.projection.GroupReadModel;
 import ttsw.filopl.todoapp.model.projection.GroupTaskReadModel;
 import ttsw.filopl.todoapp.model.projection.GroupWriteModel;
@@ -20,12 +17,11 @@ import java.util.stream.Collectors;
  **/
 
 @Service
-@RequestScope
 public class ProjectService {
-    ProjectRepository projectRepository;
-    TaskGroupRepository taskGroupRepository;
-    TaskConfigurationProperties config;
-    TaskGroupService taskGroupService;
+    private ProjectRepository projectRepository;
+    private TaskGroupRepository taskGroupRepository;
+    private TaskConfigurationProperties config;
+    private TaskGroupService taskGroupService;
 
     public ProjectService(final ProjectRepository projectRepository, final TaskGroupRepository taskGroupRepository, final TaskConfigurationProperties config, final TaskGroupService taskGroupService) {
         this.projectRepository = projectRepository;
@@ -35,31 +31,31 @@ public class ProjectService {
     }
 
     // create project
-    Project createProject(Project projectToSave) {
+    public Project save(Project projectToSave) {
         return projectRepository.save(projectToSave);
     }
 
     // read all projects
-    List<Project> readAllProjects() {
+    public List<Project> readAllProjects() {
         return projectRepository.findAll();
     }
 
     // create group
-    public GroupReadModel createGroup(int projectId, LocalDateTime deadline) {
-        if (config.getTemplate().isAllowMultipleTaskFromTemplate() == false || taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
+    public GroupReadModel createGroup(LocalDateTime deadline, int projectId) {
+        if (config.getTemplate().isAllowMultipleTaskFromTemplate() == false
+                || taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
             throw new IllegalArgumentException("Something go wrong!");
         }
         GroupReadModel result = projectRepository.findById(projectId).map(project -> {
-            var group = taskGroupService.createGroup(new GroupWriteModel());
-            group.setDescription(project.getDescription());
+            var targetGroup = taskGroupService.createGroup(new GroupWriteModel());
+            targetGroup.setDescription(project.getDescription());
 
-            group.setTasks(project.getSteps().stream().map(projectStep -> {
+            targetGroup.setTasks(project.getSteps().stream().map(projectStep -> {
                 var task = new Task(projectStep.getDescription(), deadline.plusDays(projectStep.getDaysToDeadLine()));
                 return new GroupTaskReadModel(task);
             }).collect(Collectors.toSet()));
-            return group;
+            return targetGroup;
         }).orElseThrow(() -> new IllegalArgumentException("Project with given id is not found!"));
         return result;
     }
 }
-
