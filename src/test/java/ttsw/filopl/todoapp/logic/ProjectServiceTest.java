2e3d05ef51a26db253ecb2624222ba8997be4ce0
FiloPL <tzegarlicki@gmail.com>
2022-09-27 09:14:23 +0200

    #10 - add empty test

---
+++ src/test/java/ttsw/filopl/todoapp/logic/ProjectServiceTest.java
@@ -0,0 +1,24 @@
+package ttsw.filopl.todoapp.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by T. Filo Zegarlicki on 26.09.2022
+ **/
+
+class ProjectServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void readAllProjects() {
+    }
+
+    @Test
+    void createGroup() {
+    }
+}
