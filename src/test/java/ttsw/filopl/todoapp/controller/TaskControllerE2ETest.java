d14c3cf1f000881cbede79320c35afb41e982577
FiloPL <tzegarlicki@gmail.com>
2022-10-14 08:50:21 +0200

    #13.01 - add empty e2e test class

---
+++ src/test/java/ttsw/filopl/todoapp/controller/TaskControllerE2ETest.java
@@ -0,0 +1,15 @@
+package ttsw.filopl.todoapp.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by T. Filo Zegarlicki on 14.10.2022
+ **/
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TaskControllerE2ETest {
+
+
+}
